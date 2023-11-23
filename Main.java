02b2d960a13040b4ce99eeede7880b49930d8340
21IT34 <94103928+21IT34@users.noreply.github.com>
2023-11-23 11:12:52 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -1,6 +1,6 @@
 public class Main
 {
-public static void main(string[] args)
+public static void main(String[] args)
 {
 System.out.println("Jenkin is running the Github Project ");
 System.out.println("Hello World");
