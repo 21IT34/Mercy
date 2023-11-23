24f45ed0bdab090d1f59645c199fba40f7dc99b8
21IT34 <94103928+21IT34@users.noreply.github.com>
2023-11-23 11:26:36 +0530

    Update Main.java

--- Main.java
+++ Main.java
@@ -5,11 +5,11 @@ public static void main(String[] args)
   int num=10;
   if(num %2==0)
   {
-    System.out.println("NUMBER is EVEN");
+    System.out.println("{} is EVEN"+num);
   }
   else
   {
-    System.out.println("Number is ODD");
+    System.out.println("{} is ODD"+num);
   }
 }
 }
